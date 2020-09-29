4211270df54b2c9a38fe300e62c7f1b2d2e9222a
mahmoud77732 <mm8871918@gmail.com>
2020-09-29 23:21:55 +0200

    Adding Equality in Hibernate Ex

--- src/main/java/guru/springframework/domain/Author.java
+++ src/main/java/guru/springframework/domain/Author.java
@@ -1,6 +1,7 @@
-package guru.springframework.spring5webapp.model;
+package guru.springframework.domain;
 
 import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -19,7 +20,8 @@ public class Author {
     public Author() {
     }
 
-    public Author(String firstName, String lastName, Set<Book> books) {
+    public Author(long id, String firstName, String lastName, Set<Book> books) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.books = books;
@@ -56,4 +58,28 @@ public class Author {
     public void setBooks(Set<Book> books) {
         this.books = books;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return id == author.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
 }
