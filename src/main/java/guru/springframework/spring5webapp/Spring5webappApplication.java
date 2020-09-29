ff9393fa040e58356a8ecffdc8c0c1b9e119be8f
mahmoud77732 <mm8871918@gmail.com>
2020-09-29 20:38:38 +0200

    creating the first branch

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -1,4 +1,4 @@
-package guru.springframework;
+package guru.springframework.spring5webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Spring5webappApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Spring5webappApplication.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webappApplication.class, args);
+	}
 }
